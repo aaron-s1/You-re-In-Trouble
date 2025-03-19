20bb096c556d6fc206e462e675308ec15aab2659
aaron <aaron_swan@outlook.com>
2025-03-19 07:29:32 -0700

    added To Do List

--- Assets/scripts/PissParticlePush.cs
+++ Assets/scripts/PissParticlePush.cs
@@ -8,13 +8,6 @@ public class PissParticlePush : MonoBehaviour
     {
         if (other.tag == "Player1")
             return;
-        // Revise this later so piss can't interact with parent at all.
-        // else if (gameObject.transform.parent.gameObject != null)
-        // {
-        // if (other == gameObject.transform.parent.gameObject)
-                // return;
-        // }
-
 
         Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
 
