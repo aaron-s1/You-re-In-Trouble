ca578825e7135c22d03b0200d077d54cb9cc56c1
aaron <aaron_swan@outlook.com>
2025-03-20 08:13:34 -0700

    Added shotgun weapon. cleanup.

--- Assets/scripts/PissParticlePush.cs
+++ Assets/scripts/PissParticlePush.cs
@@ -6,8 +6,23 @@ public class PissParticlePush : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        if (other.tag == "Player1")
-            return;
+        // if (other.transform.GetChild(0).TryGetComponent<ParticleSystem>(out _))
+        // {
+        //     Debug.Log($"{gameObject}'s particles hit {other}'s particles!");
+        //     // Vector2 forceDirection = (other.transform.position - transform.position).normalized;
+            
+        //     // rb.AddForce(forceDirection * pushForce, ForceMode2D.Force);            
+        //     return;
+        // }
+
+        // Debug.Log($"{gameObject} touched {other}");
+
+        // if (other.tag == "Player1")
+        // {
+        //     Debug.Log("....");            
+        //     Debug.Log($"{gameObject} touched {other}");
+        //     return;
+        // }
 
         Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
 
